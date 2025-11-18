6c7a544f1e8bb3f3869e806006e7a9f20b009d62
Simon.Weibel <simon.weibel@ost.ch>
2025-11-18 13:08:59 +0100

    add another unit test

--- src/test/java/ch/ost/hop/pipeline/transforms/wktwkb/WktWkbConverterUnitTests.java
+++ src/test/java/ch/ost/hop/pipeline/transforms/wktwkb/WktWkbConverterUnitTests.java
@@ -17,6 +17,14 @@ public class WktWkbConverterUnitTests {
     }
 
     @Test
+    public void testPolygonToWkb() throws Exception {
+        String wkt = "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))";
+        byte[] wkb = hexStringToByteArray("010300000001000000050000000000000000003E4000000000000024400000000000004440000000000000444000000000000034400000000000004440000000000000244000000000000034400000000000003E400000000000002440");
+
+        assertEquals(wkt, WktWkbConverter.wkbToWkt(wkb));
+    }
+
+    @Test
     public void testWkbToWkt() throws Exception {
         String wkt = "POINT (1 2)";
         byte[] wkb = WktWkbConverter.wktToWkb(wkt, 1);
@@ -31,4 +39,13 @@ public class WktWkbConverterUnitTests {
         assertThrows(ParseException.class, () -> WktWkbConverter.wktToWkb(wkt, 0));
     }
 
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
