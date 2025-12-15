8b7bf96d9fac03ada9d0961183b7ccd540847407
Simon.Weibel <simon.weibel@ost.ch>
2025-12-15 17:32:23 +0100

    add more unit tests

--- src/test/java/ch/ost/hop/pipeline/transforms/geometryfieldsconverter/GeometryFieldsConverterTest.java
+++ src/test/java/ch/ost/hop/pipeline/transforms/geometryfieldsconverter/GeometryFieldsConverterTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.apache.hop.core.exception.HopException;
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.*;
+
+import java.util.Arrays;
 
 public class GeometryFieldsConverterTest {
 
@@ -12,55 +14,111 @@ public class GeometryFieldsConverterTest {
   public void testWKTToGeometry() throws Exception {
     String wkt = "POINT (1 2)";
     Geometry geometry = GeometryFieldsConverter.wktToGeometry(wkt);
-    byte[] wkb = GeometryFieldsConverter.geometryToWKB(geometry, 0, false, 0);
-
-    assertNotNull(wkb);
-    assertTrue(wkb.length > 0);
+    assertNotNull(geometry);
+    assertEquals("POINT (1 2)", GeometryFieldsConverter.geometryToWKT(geometry, false, 0));
   }
 
   @Test
   public void testEWKTWithSRID() throws Exception {
     String wkt = "POINT (1 2)";
-    int srid = 4296;
+    int srid = 4326;
     Geometry geometry = GeometryFieldsConverter.wktToGeometry(wkt);
     String outWkt = GeometryFieldsConverter.geometryToWKT(geometry, true, srid);
-
-    assertNotNull(outWkt);
-    assertEquals("SRID=4296;POINT (1 2)", outWkt);
+    assertEquals("SRID=4326;POINT (1 2)", outWkt);
   }
 
   @Test
-  public void testPolygonToWkb() throws Exception {
-    String wkt = "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))";
-    byte[] wkb =
-        hexStringToByteArray(
-            "010300000001000000050000000000000000003E4000000000000024400000000000004440000000000000444000000000000034400000000000004440000000000000244000000000000034400000000000003E400000000000002440");
-    Geometry geometry = GeometryFieldsConverter.wkbToGeometry(wkb);
-
-    assertEquals(wkt, GeometryFieldsConverter.geometryToWKT(geometry, false, 0));
-  }
-
-  @Test
-  public void testEWKTToEWKBAndBack() throws Exception {
-    String ewkt = "SRID=4326;POINT (-44.3 60.1)";
+  public void testEWKTAlreadyHasSRID_AddFalse() throws Exception {
+    String ewkt = "SRID=3857;POINT (10 20)";
     Geometry geometry = GeometryFieldsConverter.wktToGeometry(ewkt);
-    String result = GeometryFieldsConverter.geometryToWKT(geometry, false, 0);
-    assertEquals(ewkt, result);
+    String outWkt = GeometryFieldsConverter.geometryToWKT(geometry, false, 0);
+    assertEquals("SRID=3857;POINT (10 20)", outWkt);
   }
 
   @Test
-  public void testFalseWKT() {
-    String wkt = "test string you're supposed to fail";
+  public void testMalformedSRIDThrows() {
+    String ewkt = "SRID=abcd;POINT (1 2)";
+    assertThrows(HopException.class, () -> GeometryFieldsConverter.wktToGeometry(ewkt));
+  }
+
+  @Test
+  public void testInvalidWKTThrows() {
+    String wkt = "INVALIDSTRING";
     assertThrows(HopException.class, () -> GeometryFieldsConverter.wktToGeometry(wkt));
   }
 
-  public static byte[] hexStringToByteArray(String s) {
-    int len = s.length();
-    byte[] data = new byte[len / 2];
-    for (int i = 0; i < len; i += 2) {
-      data[i / 2] =
-          (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+  @Test
+  public void testPointWKBRoundtrip() throws Exception {
+    Geometry geometry = GeometryFieldsConverter.pcToGeometry(12.34, 56.78);
+    byte[] wkbBig = GeometryFieldsConverter.geometryToWKB(geometry, 1, false, 0);
+    byte[] wkbLittle = GeometryFieldsConverter.geometryToWKB(geometry, 0, false, 0);
+
+    assertFalse(Arrays.equals(wkbBig, wkbLittle));
+    Geometry fromBig = GeometryFieldsConverter.wkbToGeometry(wkbBig);
+    Geometry fromLittle = GeometryFieldsConverter.wkbToGeometry(wkbLittle);
+    assertEquals(geometry.getCoordinate().getX(), fromBig.getCoordinate().x);
+    assertEquals(geometry.getCoordinate().getY(), fromBig.getCoordinate().y);
+    assertEquals(geometry.getCoordinate().getX(), fromLittle.getCoordinate().x);
+    assertEquals(geometry.getCoordinate().getY(), fromLittle.getCoordinate().y);
+  }
+
+  @Test
+  public void testInvalidWKBThrows() {
+    byte[] invalidWKB = new byte[] {1, 2, 3, 4};
+    assertThrows(HopException.class, () -> GeometryFieldsConverter.wkbToGeometry(invalidWKB));
+  }
+
+  @Test
+  public void testPointCoordinateRoundtrip() {
+    double x = 5.5;
+    double y = -3.3;
+    Geometry geometry = GeometryFieldsConverter.pcToGeometry(x, y);
+    double[] coords = GeometryFieldsConverter.geometryToPC(geometry);
+    assertEquals(x, coords[0]);
+    assertEquals(y, coords[1]);
+  }
+
+  @Test
+  public void testPointCoordinateWithZ() {
+    Coordinate coord = new Coordinate(1, 2, 3);
+    Geometry geometry = new GeometryFactory().createPoint(coord);
+    double[] coords = GeometryFieldsConverter.geometryToPC(geometry);
+    assertEquals(1, coords[0]);
+    assertEquals(2, coords[1]);
+  }
+
+  @Test
+  public void testNullCoordinatesReturnNullGeometry() throws HopException {
+    Object[] inputRow = new Object[] {null, null};
+    Geometry geometry = null;
+    try {
+      geometry = GeometryFieldsConverter.pcToGeometry((Double) inputRow[0], (Double) inputRow[1]);
+    } catch (NullPointerException e) {
     }
-    return data;
+    assertNull(geometry);
+  }
+
+  @Test
+  public void testPolygonWKTRoundtrip() throws Exception {
+    String wkt = "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))";
+    Geometry geometry = GeometryFieldsConverter.wktToGeometry(wkt);
+    String outWkt = GeometryFieldsConverter.geometryToWKT(geometry, false, 0);
+    assertEquals(wkt, outWkt);
+  }
+
+  @Test
+  public void testLineStringWKTRoundtrip() throws Exception {
+    String wkt = "LINESTRING (0 0, 1 1, 2 2)";
+    Geometry geometry = GeometryFieldsConverter.wktToGeometry(wkt);
+    String outWkt = GeometryFieldsConverter.geometryToWKT(geometry, false, 0);
+    assertEquals(wkt, outWkt);
+  }
+
+  @Test
+  public void testEWKTWithSRIDZero() throws Exception {
+    String ewkt = "SRID=0;POINT (7 8)";
+    Geometry geometry = GeometryFieldsConverter.wktToGeometry(ewkt);
+    String outWkt = GeometryFieldsConverter.geometryToWKT(geometry, false, 0);
+    assertEquals("POINT (7 8)", outWkt);
   }
 }
